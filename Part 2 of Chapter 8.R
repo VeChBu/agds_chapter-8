eef2b9f0a7f284e93131b159eb1645588f0e151e
VeChBu <vera.buenzli@students.unibe.ch>
2025-03-31 15:30:21 +0200

    Add new file with a poem

---
+++ Part 2 of Chapter 8.R
@@ -0,0 +1,5 @@
+"„Ich will Ihnen ein Geheimnis verraten. […] Wir lesen und schreiben Gedichte nicht nur so zum Spaß. Wir lesen und schreiben Gedichte, weil wir zur Spezies Mensch zählen, und die Spezies Mensch ist von Leidenschaft erfüllt; und Medizin, Jura, Wirtschaft und Technik sind zwar durchaus edle Ziele und auch notwendig; aber Poesie, Schönheit, Romantik, Liebe sind die Freuden unseres Lebens.
+
+Ich möchte an dieser Stelle Whitman zitieren, ‚Ich und mein Leben … die immer wiederkehrenden Fragen, der endlose Zug der Ungläubigen, die Städte voller Narren. Wozu bin ich da? Wozu nützt dieses Leben? Die Antwort. Damit Du hier bist. Damit das Leben nicht zu Ende geht. Deine Individualität. Damit das Spiel der Mächte weiterbesteht und Du deinen Vers dazu beitragen kannst.‘“
+
+Quelle: Der fiktive Charakter John Keating (Robin Williams)"
